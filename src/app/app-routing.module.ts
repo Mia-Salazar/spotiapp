35ef33c6a8e9ea4ab978ca431e488cb44cc1b6da
Mia-Salazar <maria.sgr@gmail.com>
2020-08-24 19:48:15 +0200

    Create navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { ArtistComponent } from './components/artist/artist.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'contact', component: SearchComponent},
-  {path: 'portfolio', component: ArtistComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'artist', component: ArtistComponent},
   {path: 'routePath', component: HomeComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
